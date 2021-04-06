92c025399ecac37b6a3299c10b5b01effb865829
wangyahao <yahaowang@rdchina.net>
2021-04-06 22:50:22 +0800

    -feat MyInstantiationAwareBeanPostProcessor

---
+++ bean-lifecycle/src/main/java/com/wyh/think/in/spring/bean/lifecycle/MyInstantiationAwareBeanPostProcessor.java
@@ -0,0 +1,24 @@
+package com.wyh.think.in.spring.bean.lifecycle;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.PropertyValues;
+import org.springframework.beans.factory.config.InstantiationAwareBeanPostProcessor;
+
+public class MyInstantiationAwareBeanPostProcessor implements InstantiationAwareBeanPostProcessor {
+
+    // Bean初始化前
+    @Override
+    public Object postProcessBeforeInstantiation(Class<?> beanClass, String beanName) throws BeansException {
+        return null;
+    }
+
+    @Override
+    public boolean postProcessAfterInstantiation(Object bean, String beanName) throws BeansException {
+        return false;
+    }
+
+    @Override
+    public PropertyValues postProcessProperties(PropertyValues pvs, Object bean, String beanName) throws BeansException {
+        return null;
+    }
+}
---
+++ bean-lifecycle/src/main/java/com/wyh/think/in/spring/bean/lifecycle/UserHolder.java
@@ -0,0 +1,14 @@
+package com.wyh.think.in.spring.bean.lifecycle;
+
+public class UserHolder {
+
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
